ae54a4ef2086ff645c0600182847d248e79fc014
Ansih Singh Walia <loveeanish@gmail.com>
2017-07-20 01:36:08 +0530

    Fucntions modified

--- decrypt.cpp
+++ decrypt.cpp
@@ -8,18 +8,24 @@ using namespace std;
 
 char decrypt(char text[100],int key)
 {
-	int p=0,encryptkey;
+	int p=0;
+	for(key = 0 ; key < 26 ; key ++) //this loop will check for all possible combinations 
+    {
+	
 	for(int i = 0 ; i<strlen(text);i++)
 	{
-		p = int(text[i]);
-		p = (p-key);
+		p = int(text[i]);//stores the ascii values
+		p = (p-key);//applying the shifting technique
 		text[i] = char(p); 
 		
 		
-	}
+	}//end for
 	cout<<"The message is decrypted as :"<<text<<endl;
 	
-}
+   }//end for      
+
+}//end decrypt
+
 
 int main()
 {
