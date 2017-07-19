efee97f4335d1ba4c8fcd8ac6fdfabe3be43d527
Ansih Singh Walia <loveeanish@gmail.com>
2017-07-20 01:22:45 +0530

    Decryption Program executed

--- decrypt.cpp
+++ decrypt.cpp
@@ -8,8 +8,32 @@ using namespace std;
 
 char decrypt(char text[100],int key)
 {
+	int p=0,encryptkey;
+	for(int i = 0 ; i<strlen(text);i++)
+	{
+		p = int(text[i]);
+		p = (p-key);
+		text[i] = char(p); 
+		
+		
+	}
+	cout<<"The message is decrypted as :"<<text<<endl;
 	
+}
+
+int main()
+{
+	int key ;
+	char text[100];
 	
+	cout<<"Enter the Encrypted Text"<<endl;
+	cin>>text;
+	cout<<"Enter the Ciphers shift"<<endl;
+	cin>>key;
 	
+	decrypt(text,key);
 	
+
+	
+return 0 ;
 }
