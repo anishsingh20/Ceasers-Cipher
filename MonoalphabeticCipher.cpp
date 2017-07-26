66e45ae684d0d3aaf38806de75b6aff2aca0c2b5
Ansih Singh Walia <loveeanish@gmail.com>
2017-07-27 00:09:32 +0530

    Implementing Monoalphabetic ciphers in C++

---
+++ MonoalphabeticCipher.cpp
@@ -0,0 +1,39 @@
+//monoalphabetic cipher
+//monoalphabatic ciphers are a form of Substitution ciphers which uses fixed substitution over the entire message 
+#include<iostream>
+#include<string.h>
+#include<cctype>
+
+
+
+int main()
+{
+	int choice;
+	string plaintext,ciphertext;
+
+	cout<<"------------Enter your choice-----------"<<endl;
+	cout<<"\n1)Encrypt\n2)Decrypt\n3)Exit"<<endl;
+	cin>>choice;
+	
+	for(;;)
+	{
+		switch(choice)
+		{	//encrypt case
+			case 1:
+			   continue;
+			case 2:
+			   continue;
+			case 3:
+			   break;	
+		}
+		
+	break;	
+	}//end for
+	
+
+	
+	
+	
+	
+return 0;	
+}
