15a1a8735053537989b1d116053bf5a259589874
Ansih Singh Walia <loveeanish@gmail.com>
2017-07-27 00:46:02 +0530

    Encryption code added

--- MonoalphabeticCipher.cpp
+++ MonoalphabeticCipher.cpp
@@ -1,25 +1,45 @@
 //monoalphabetic cipher
 //monoalphabatic ciphers are a form of Substitution ciphers which uses fixed substitution over the entire message 
 #include<iostream>
-#include<string.h>
+#include<string>
 #include<cctype>
+#include<stdlib.h>
+
+using namespace std;
 
 
 
 int main()
 {
-	int choice;
-	string plaintext,ciphertext;
+	int choice,ascii,cpt;
+	string plaintext;
+	string ciphertext;
 
-	cout<<"------------Enter your choice-----------"<<endl;
-	cout<<"\n1)Encrypt\n2)Decrypt\n3)Exit"<<endl;
-	cin>>choice;
 	
 	for(;;)
 	{
+	cout<<"------------Enter your choice-----------"<<endl;
+	cout<<"\n1)Encrypt\n2)Decrypt\n3)Exit"<<endl;
+	cin>>choice;	
 		switch(choice)
 		{	//encrypt case
 			case 1:
+			    cout<<"Enter the plainText"<<endl;
+			    cin>>ws;
+			    getline(cin,plaintext); //to consider spaces in string input
+			    
+			    for(unsigned i = 0 ; i<plaintext.size(); i++)
+			    {
+			    	if(isalpha(plaintext[i])) 
+				    {
+				    
+			    	    ascii = int(plaintext[i]); //storing the ascii value of the string
+			    	    cpt = (ascii + 1);//cipher text in ascii value
+			    	    plaintext[i] = char(cpt);//converting ascii to character value
+			    	    }//end if
+			    }//end for
+			    cout<<"The cipher Text is : "<<plaintext<<endl;//the cipher text
+			     	
 			   continue;
 			case 2:
 			   continue;
