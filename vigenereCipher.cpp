b5c59a9136ef52e8b2bebdf6ece4dc6176fa7184
Ansih Singh Walia <loveeanish@gmail.com>
2017-08-04 13:40:54 +0530

    encryption function made

--- vigenereCipher.cpp
+++ vigenereCipher.cpp
@@ -1,8 +1,49 @@
 #include<iostream>
-#include<string.h>
+#include<string>
 #include<cctype>
 
+
+using namespace std;
 //program to encrypt a plaintext message using a VIGENERE cipher which is a polyalphabetic substitution technique which
 //uses different letters for to encrypt each alphabet in plaintext.
 
-using namespace std;
+//function to take key as input
+string key(string str, string key)
+{
+	int x = str.size();
+	for(int i=0;;i++)
+	{
+		if(x==i)
+		   i=0;
+		if(key.size() == str.size() ) //if length of string and key matches then come out
+		   break;
+		key.push_back(key[i]); 
+		
+	}
+	return key;
+} //function to generate a key
+
+
+//function to encrypt the plaintext
+string encrypt(string str , string key)
+{
+	string cipherText;
+	for(int i=0;i<str.size();i++)
+	{
+		int x = (str[i] + key[i])%26;
+		cout<<x<<endl;
+		
+		x += 'A'; //converting to alphabets values
+		cipherText.push_back(x);
+		
+	}
+	return cipherText;
+}
+
+
+int main()
+{	//an array of alaphabets which will be used in substitution
+
+	
+return 0;
+}
